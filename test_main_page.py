a6e2de2d653f58f716be0bdc18df7ba8913f1de5
SaleevaMariia <sunlimen13@gmail.com>
2020-05-10 18:46:19 +0300

    add transition between main and login pages

--- test_main_page.py
+++ test_main_page.py
@@ -19,21 +19,27 @@ def test_guest_should_see_login_link(browser):
 
 
 def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_form()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_form()
 
 
 def test_guest_should_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_register_form()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_register_form()
 
 
 def test_guest_login_page_url_should_contain_login(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_url()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()
